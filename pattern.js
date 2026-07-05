268ae3603594f53c3a492f8d08259d29f4360359
Akshay <720akshaykumar@gmail.com>
2026-07-05 21:38:33 +0530

    this is my last commit

---
+++ pattern.js
@@ -0,0 +1,15 @@
+// Print Right-Angled Star Triangle
+
+let star = 5;
+
+for(let i = 1; i<=star; i++){
+    let str = '';
+
+    for(let j = 1; j<=i; j++){
+        str = str + '*'
+    }
+    console.log(str);
+
+}
+
+// Print Inverted Right-Angled Triangle
