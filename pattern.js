391503a7027137076d5128ff5975d7b4dc11a6dc
Akshay <720akshaykumar@gmail.com>
2026-07-08 17:13:43 +0530

    this is pattern frm day 3

--- pattern.js
+++ pattern.js
@@ -1,15 +1,68 @@
 // Print Right-Angled Star Triangle
 
-let star = 5;
+// let star = 5;
 
-for(let i = 1; i<=star; i++){
-    let str = '';
+// for(let i = 1; i<=star; i++){
+//     let str = '';
 
-    for(let j = 1; j<=i; j++){
-        str = str + '*'
-    }
-    console.log(str);
+//     for(let j = 1; j<=i; j++){
+//         str = str + '*'
+//     }
+//     console.log(str);
 
-}
+// }
 
 // Print Inverted Right-Angled Triangle
+// Print Inverted Right-Angled Triangle
+
+// let star = 5;
+
+// for(let i = star;  i>=1; i--){
+//     let str = ''
+
+//     for(let j =  i; j>= 1; j-- ){
+//         str += '*'
+//     }
+
+//     console.log(str);
+// }
+//  Print Pyramid Pattern
+
+
+// let star = 5;
+
+// for (let i = 1; i <= star; i++) {
+
+//     let star2 = "";
+
+//     // Spaces
+//     for (let j = 1; j <= star - i; j++) {
+//         star2 += " ";
+//     }
+
+//     // Stars
+//     for (let k = 1; k <= (2 * i) - 1; k++) {
+//         star2 += "*";
+//     }
+
+//     console.log(star2);
+// }
+
+
+//  Print Inverted Pyramid Pattern
+
+// let star = 5;
+
+// for(let i = star; i>=1; i--){
+//     let row = '';
+
+//         for(let j = 1; j<=star-i; j++){
+//             row += ' '
+//         }
+
+//         for(let k = 1; k<= (2*i) -1 ; k++){
+//             row += "*"
+//         }
+
+//         console.log(row);
+// }
